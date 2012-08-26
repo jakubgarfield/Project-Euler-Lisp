8dcab9175b0bb474b576c56cd54ac6fbf1937b27
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2012-08-26 20:33:13 +0200

    Problem 001 solved

--- src/problem-001.rkt
+++ src/problem-001.rkt
@@ -6,9 +6,19 @@
 (define problem-001
   (class* problem (problem-interface)
     (super-new)
+    (define/override-final (description)
+      "Add all the natural numbers below one thousand that are multiples of 3 or 5.")
     (define/override-final (solve-implementation)
-      400514)))
+      (multipliers-below 999 0))))
+
+(define (multipliers-below counter acc)
+  (cond [(eq? counter 0) acc]
+        [(or (is-divisible counter 3)
+             (is-divisible counter 5)) (multipliers-below (- counter 1) (+ acc counter))]
+        [else (multipliers-below (- counter 1) acc)]))
+
+(define (is-divisible value factor)
+  (eq? (modulo value factor) 0))
 
 
-
-(send (new problem-001) print-result)
+(send (new problem-001) print-problem)
--- src/problem.rkt
+++ src/problem.rkt
@@ -13,8 +13,11 @@
       (let ([current (current-milliseconds)]
             [result (send this solve-implementation)])
         (values result (- (current-milliseconds) current))))
+    (define/public (description)
+      (error "Override description method"))
     (define/public (solve-implementation)
       (error "Override solve method"))
-    (define/public (print-result)
+    (define/public (print-problem)
+      (fprintf (current-output-port) (send this description))
       (let-values (((result time) (send this solve)))
-            (format "Result is ~a and was computed in ~b seconds" result time)))))
+        (fprintf (current-output-port) "~nResult is ~a and was computed in ~b milliseconds." result time)))))
