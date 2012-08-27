71a46f8bc38ee98c55971eb12b084d27bc7a33d7
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2012-08-27 23:31:33 +0200

    Problem 002 solved

--- src/problem-002.rkt
+++ src/problem-002.rkt
@@ -9,8 +9,17 @@
     (define/override-final (description)
       "By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.")
     (define/override-final (solve-implementation)
-      0)))
+      (fibonacci-sum 1 1 4000000 is-even 0))))
 
+(define (fibonacci-next previous current)
+  (+ previous current))
 
+(define (is-even value)
+  (eq? (modulo value 2) 0))
+
+(define (fibonacci-sum previous current max condition acc)
+  (cond [(> current max) acc]
+        [else (fibonacci-sum current (fibonacci-next previous current) max condition (cond [(condition current) (+ acc current)]
+                                                                                           [else acc]))]))
 
 (send (new problem-002) print-problem)
